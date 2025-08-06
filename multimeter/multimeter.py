5cd051ff0d70091cfab1a2df951a1f3191ea007e
Peter <peter@quantr.hk>
2025-08-07 01:50:45 +0800

    dev

--- multimeter/multimeter.py
+++ multimeter/multimeter.py
@@ -10,13 +10,15 @@ from machine import Pin
 import AD9833
 
 selected = 0
+current_freq = 1300
+
 
 def drawMenu():
     button1 = lv.button(scrn)
-    button1.set_pos(4, 30)
+    button1.set_pos(0, 30)
     button1.set_size(40, 20)
     label1 = lv.label(button1)
-    label1.set_text("Func")
+    label1.set_text("SQU")
     label1.set_style_text_color(lv.color_hex(0x000000), 0)  # Black text
     label1.set_style_text_font(lv.font_montserrat_12, 0)
     if selected == 0:
@@ -25,16 +27,52 @@ def drawMenu():
     label1.center()
 
     button2 = lv.button(scrn)
-    button2.set_pos(50, 30)
-    button2.set_size(75, 20)
+    button2.set_pos(43, 30)
+    button2.set_size(40, 20)
     label2 = lv.label(button2)
-    label2.set_text("Multimeter")
+    label2.set_text("Tri")
     label2.set_style_text_color(lv.color_hex(0x000000), 0)  # Black text
     label2.set_style_text_font(lv.font_montserrat_12, 0)
     if selected == 1:
         button2.set_style_bg_color(
             lv.color_hex(0xffffff), 0)
     label2.center()
+
+    button3 = lv.button(scrn)
+    button3.set_pos(86, 30)
+    button3.set_size(40, 20)
+    label3 = lv.label(button3)
+    label3.set_text("SINE")
+    label3.set_style_text_color(lv.color_hex(0x000000), 0)  # Black text
+    label3.set_style_text_font(lv.font_montserrat_12, 0)
+    if selected == 2:
+        button3.set_style_bg_color(
+            lv.color_hex(0xffffff), 0)
+    label3.center()
+
+    # --- Add 6 frequency adjustment buttons below ---
+    freq_btn_labels = ["-1k", "-100", "-10", "+10", "+100", "+1k"]
+    freq_btn_steps = [-1000, -100, -10, 10, 100, 1000]
+    global freq_buttons
+    freq_buttons = []
+    for i, label in enumerate(freq_btn_labels):
+        btn = lv.button(scrn)
+        btn.set_pos(2 + i*21, 60)
+        btn.set_size(20, 20)
+        lbl = lv.label(btn)
+        lbl.set_text(label)
+        lbl.set_style_text_color(lv.color_hex(0x000000), 0)
+        lbl.set_style_text_font(lv.font_montserrat_12, 0)
+        lbl.center()
+        freq_buttons.append(btn)
+
+    # --- Display frequency in next row ---
+    global freq_label
+    freq_label = lv.label(scrn)
+    freq_label.set_pos(10, 85)
+    freq_label.set_style_text_color(lv.color_hex(0xffffff), 0)
+    freq_label.set_style_text_font(lv.font_montserrat_16, 0)
+    freq_label.set_text(f"{current_freq} Hz")
 
 
 # display settings
@@ -106,7 +144,7 @@ img.set_pos(0, 5)
 
 
 label = lv.label(scrn)
-label.set_text("Multimeter")
+label.set_text("Func Generator")
 label.set_pos(24, 8)
 label.set_style_text_color(lv.color_hex(0xffffff), 0)
 label.set_style_text_font(lv.font_montserrat_12, 0)
@@ -119,11 +157,11 @@ drawMenu()
 # temp.value(1)
 # display_bus.deinit()
 
-ad9833 = AD9833.AD9833(sdo = 21, clk = 22, cs = 2,  fmclk = 25)
+ad9833 = AD9833.AD9833(sdo=21, clk=22, cs=2,  fmclk=25)
 ad9833.set_frequency(1300, 0)
 # ad9833.set_frequency(2600, 1)
-ad9833.set_phase(0, 0, rads = False)
-ad9833.set_phase(180, 1, rads = False)
+ad9833.set_phase(0, 0, rads=False)
+ad9833.set_phase(180, 1, rads=False)
 
 time.sleep(0.5)
 
@@ -143,17 +181,21 @@ while True:
     time.sleep_ms(20)
     lv.task_handler()
     sleep(0.2)
-    if not button0.value():
-        selected = (selected + 1) % 2
+    if not button0.value():  # Button pressed
+        selected = (selected - 1) % 3
         print(selected)
         drawMenu()
-        lv.refr_now(lv.screen_active().get_display())
+    if not button1.value():  # Button pressed
+        selected = (selected + 1) % 3
+        print(selected)
+        drawMenu()
+    lv.refr_now(lv.screen_active().get_display())
+    if selected == 0:
+        ad9833.set_frequency(9000, 0)
+        ad9833.set_mode('SQUARE')
+    if selected == 1:
+        ad9833.set_frequency(1300, 0)
+        ad9833.set_mode('TRIANGLE')
+    if selected == 2:
         # ad9833.select_freq_phase(0,0)
         ad9833.set_mode('SIN')
-    if not button1.value():
-        selected = (selected - 1) % 2
-        print(selected)
-        drawMenu()
-        lv.refr_now(lv.screen_active().get_display())
-        ad9833.set_frequency(9000, 0)
-        ad9833.set_mode('SQUARE/2')
